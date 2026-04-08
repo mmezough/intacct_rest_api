2fd49b885d3f15694040c3b8ddcb3a53a466c5f3
momez <momez@DESKTOP-K4QFGD6>
2026-04-08 10:51:58 +0200

    lesson-5: remove composite keep batch+bulk

--- Services/IntacctService.cs
+++ Services/IntacctService.cs
@@ -1,7 +1,6 @@
 using intacct_rest_api.Models;
 using intacct_rest_api.Models.Bulk;
 using intacct_rest_api.Models.Batch;
-using intacct_rest_api.Models.Composite;
 using intacct_rest_api.Models.Export;
 using intacct_rest_api.Models.InvoiceCreate;
 using intacct_rest_api.Models.BillLineUpdate;
@@ -263,17 +262,6 @@ public class IntacctService
         var restRequest = new RestRequest($"{NormalizeObjectPath(objectPath)}/{joinedKeys}", Method.Delete);
         restRequest.AddHeader("Authorization", "Bearer " + accessToken);
         ApplyAtomicHeader(restRequest, atomic);
-        return await _client.ExecuteAsync(restRequest);
-    }
-
-    /// <summary>
-    /// Envoie une requête composite (plusieurs sous-requêtes en un seul POST). POST /services/core/composite.
-    /// </summary>
-    public async Task<RestResponse> Composite(List<CompositeSubRequest> subRequests, string accessToken)
-    {
-        var restRequest = new RestRequest("services/core/composite", Method.Post);
-        restRequest.AddHeader("Authorization", "Bearer " + accessToken);
-        restRequest.AddStringBody(SerializeBody(subRequests), DataFormat.Json);
         return await _client.ExecuteAsync(restRequest);
     }
 
